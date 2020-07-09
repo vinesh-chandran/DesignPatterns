7f1a210954417d92d3781ce2e3130721932b6b04
vinesh <vineshchandran.cad@gmail.com>
2020-07-09 15:55:59 +0100

    Folder changed to Creational pattern folder.

---
+++ CreationalPattern/ProtoType/ProtoType/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Developer dev = new Developer();
+            dev.Name = "Rahul";
+            dev.Role = "Team Leader";
+            dev.PreferredLanguage = "C#";
+
+            Developer devCopy = (Developer)dev.Clone();
+            devCopy.Name = "Arif"; //Not mention Role and PreferredLanguage, it will copy above
+
+            Console.WriteLine(dev.GetDetails());
+            Console.WriteLine(devCopy.GetDetails());
+
+            Typist typist = new Typist();
+            typist.Name = "Monu";
+            typist.Role = "Typist";
+            typist.WordsPerMinute = 120;
+
+            Typist typistCopy = (Typist)typist.Clone();
+            typistCopy.Name = "Sahil";
+            typistCopy.WordsPerMinute = 115;//Not mention Role, it will copy above
+
+            Console.WriteLine(typist.GetDetails());
+            Console.WriteLine(typistCopy.GetDetails());
+
+            Console.ReadKey();
+        }
+    }
+}
